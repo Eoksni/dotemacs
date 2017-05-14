bb39e01b02dbe7a2daf7cbfe0a45dd10394e22b3
Dmitry Mazurok <mazurok@outlook.com>
2017-05-14 17:08:03 +0700

    introduced machine-specific configuration

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -318,10 +318,14 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
-  (setq magit-repository-directories '("E:/eoksni-dir/work/jslearning"))
   (setq js2-strict-trailing-comma-warning nil)
-  )
- 
+  (spacemacs/toggle-highlight-current-line-globally-off)
+
+  (funcall (intern (concat "dmaz-" system-name "/user-config"))))
+
+(defun dmaz-eoksni-win8/user-config ()
+  (setq magit-repository-directories '("E:/eoksni-dir/work/jslearning")))
+
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
 (custom-set-variables
