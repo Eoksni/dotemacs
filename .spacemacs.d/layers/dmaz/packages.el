cfcc819f148caea6d3208800281cc1735f1b39b1
Dmitry Mazurok <mazurok@outlook.com>
2017-05-23 00:18:35 +0700

    wtf

--- .spacemacs.d/layers/dmaz/packages.el
+++ .spacemacs.d/layers/dmaz/packages.el
@@ -2,15 +2,20 @@
   '(
     ranger
     typescript-mode
+    git-commit-insert-issue
     ))
 
 (defun dmaz/post-init-ranger ()
   ;; https://github.com/ralesi/ranger.el/issues/156
   ;; Fix the masks for the elisp internal implementation of ls
-  (setq ranger-dired-display-mask '(t t t t t t t)
-        ranger-dired-hide-mask '(nil nil nil nil nil nil t))
+  ;; (setq ranger-dired-display-mask '(t t t t t t t)
+  ;;       ranger-dired-hide-mask '(nil nil nil nil nil nil t))
   )
 
 (defun dmaz/pre-init-typescript-mode ()
   (use-package typescript-mode
     :mode "\\.js\\'"))
+
+(defun dmaz/init-git-commit-insert-issue ()
+  (use-package git-commit-insert-issue
+    :defer t))
