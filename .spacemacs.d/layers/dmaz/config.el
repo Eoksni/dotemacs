a6c649bd35c41305facd73c45c67b058a9ee3669
Dmitry Mazurok <mazurok@outlook.com>
2017-06-03 06:51:30 +0700

    updated for laptop usage

--- .spacemacs.d/layers/dmaz/config.el
+++ .spacemacs.d/layers/dmaz/config.el
@@ -1,16 +1,13 @@
 (defun dmaz/config-eoksni-win8 ()
-  (with-eval-after-load "ispell"
-    (setenv "DICPATH" "E:/eoksni-dir/portable/hunspell_dictionaries")
-    (setq ispell-program-name "hunspell")
-    (setq ispell-dictionary "een_US,ru_RU")
-    ;; ispell-set-spellchecker-params has to be called
-    ;; before ispell-hunspell-add-multi-dic will work
-    (ispell-set-spellchecker-params)
-    (ispell-hunspell-add-multi-dic "een_US,ru_RU")))
+  )
+
+(defun dmaz/config-eoksni-zen-win8 ()
+  (setq dmaz/eoksni-dir "C:\\eoksni-dir")
+  (setq dmaz/dropbox-dir "C:\\Users\Dmitriy\Dropbox"))
+
 (setq-default flyspell-generic-check-word-predicate
               #'flyspell-generic-progmode-verify)
 (advice-add 'ispell-comments-and-strings :override #'dmaz/ispell-comments-and-strings)
-;; (advice-add 'move-beginning-of-line :around #'dmaz/move-beginning-of-line--smart)
 (advice-add 'typescript/format :around #'dmaz/typescript/format--tmp-here)
 (setq projectile-test-suffix-function #'dmaz/projectile-test-suffix)
 (setq projectile-create-missing-test-files t)
@@ -19,5 +16,14 @@
 (defvar dmaz/growlnotify-command "C:\\Program Files (x86)\\Growl for Windows\\growlnotify.exe")
 (defvar dmaz/eoksni-dir "E:\\eoksni-dir")
 (defvar dmaz/dropbox-dir "H:\\Documents\\Dropbox")
+
+(with-eval-after-load "ispell"
+  (setenv "DICPATH" (dmaz/joindirs dmaz/eoksni-dir "portable/hunspell_dictionaries"))
+  (setq ispell-program-name "hunspell")
+  (setq ispell-dictionary "een_US,ru_RU")
+  ;; ispell-set-spellchecker-params has to be called
+  ;; before ispell-hunspell-add-multi-dic will work
+  (ispell-set-spellchecker-params)
+  (ispell-hunspell-add-multi-dic "een_US,ru_RU"))
 
 (funcall (intern (concat "dmaz/config-" (downcase (system-name)))))
--- README.md
+++ README.md
@@ -4,23 +4,55 @@ This is my dotemacs - configuration directory for Emacs. Super Cool!
 
 ## Prerequisites (Windows 10)
 
+For search
+
 ```bat
 choco install ripgrep
+```
+
+For spell checking
+
+```bat
 choco install hunspell.portable
+```
+
+For find
+
+```bat
+choco install gnuwin32
 ```
 
 For markdown
 
 ```bat
 choco install pandoc
+choco install ruby
 gem install mdl
 ```
 
-For emacs benchmark
+For emacs benchmark (dont need if used unison to get this repo)
 
 ```bat
 cd benchmark-init-git
 make
 ```
 
+### Other requirements
 
+#### Growl
+
+Install [Growl](http://www.growlforwindows.com)
+
+Then install [GrowlToToast](https://github.com/BobVul/GrowlToToast)
+
+#### Dropbox
+
+Install [Dropbox](http://www.dropbox.com)
+
+#### Everything
+
+Install [Everything](https://www.voidtools.com/ru-ru/) or
+
+```bat
+choco install everything
+```
