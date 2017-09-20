b230aec2a72b1afc4f6b1be3583315df39fe3005
Dmitry Mazurok <mazurok@outlook.com>
2017-09-21 00:42:08 +0700

    fixed duplicate work file

--- .spacemacs.d/layers/dmaz-org/funcs.el
+++ .spacemacs.d/layers/dmaz-org/funcs.el
@@ -20,7 +20,8 @@ simple timestamp string."
 
   (defun dmaz-org/setup-eoksni-dir ()
     (add-to-list 'org-agenda-files (dmaz/joindirs dmaz/eoksni-dir "notes"))
-    (add-to-list 'org-agenda-files dmaz-org/work-agenda-file))
+    ;; (add-to-list 'org-agenda-files dmaz-org/work-agenda-file)
+    )
 
   (defun dmaz-org/setup-dropbox-dir ()
     ;; (setq org-directory (dmaz/joindirs (file-name-as-directory dmaz/dropbox-dir) "notes"))
